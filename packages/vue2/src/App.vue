62b82ece5b40d8d8da03591cef1527979138dadd
chenshenhai <cshenhai@gmail.com>
2022-01-21 22:05:28 +0800

    feat: add vue2 example

---
+++ packages/vue2/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="app">
+    <div ref="mycanvas"></div>
+  </div>
+</template>
+
+<script>
+import iDraw from 'idraw';
+
+export default {
+  name: 'App',
+  components: {},
+  methods: {},
+  mounted() {
+    const idraw = new iDraw(this.$refs.mycanvas, {
+      width: 600,
+      height: 400,
+      contextWidth: 600,
+      contextHeight: 400,
+      devicePixelRatio: 1,
+    });
+    idraw.addElement({
+      name: "rect-001",
+      x: 140,
+      y: 120,
+      w: 200,
+      h: 100,
+      type: "rect",
+      desc: {
+        bgColor: "#f7d3c1",
+        borderRadius: 20,
+        borderWidth: 4,
+        borderColor: "#ff6032",
+      },
+    })
+  }
+}
+</script>
+
+<style>
+.app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+canvas {
+  border: 1px solid #888888;
+}
+</style>
